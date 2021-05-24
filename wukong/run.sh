a26cf77dd4555df610919a3f6f8d85c4f614a893
iYongHsu <337821840@qq.com>
2021-05-24 13:31:30 +0800

    Update run.sh

--- wukong/run.sh
+++ wukong/run.sh
@@ -1,6 +1,14 @@
 alias cp='cp'
-wget http://127.0.0.1:8899/share/wukongdata/config.yml
-wget http://127.0.0.1:8899/share/wukongdata/wukong.pmdl
-cp -f config.yml /root/.wukong/config.yml
-cp -f wukong.pmdl /root/.wukong/wukong.pmdl
+if [ ! -f "/root/.wukong/config.yml" ]; then
+  wget http://127.0.0.1:8899/share/wukongdata/config.yml
+  cp -f config.yml /root/.wukong/config.yml
+fi
+if [ ! -f "/root/.wukong/wukong.pmdl" ]; then
+  wget http://127.0.0.1:8899/share/wukongdata/xiaotianquan.pmdl
+  cp -f wukong.pmdl /root/.wukong/xiaotianquan.pmdl
+fi
+# wget http://127.0.0.1:8899/share/wukongdata/config.yml
+# wget http://127.0.0.1:8899/share/wukongdata/wukong.pmdl
+# cp -f config.yml /root/.wukong/config.yml
+# cp -f wukong.pmdl /root/.wukong/wukong.pmdl
 python3 wukong.py
