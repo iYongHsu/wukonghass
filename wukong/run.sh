1d303bcf68bfe791fe6bb30a6f782ac49c34f23d
iYongHsu <337821840@qq.com>
2021-05-25 19:16:30 +0800

    Update run.sh

--- wukong/run.sh
+++ wukong/run.sh
@@ -3,9 +3,9 @@ if [ ! -f "/root/.wukong/config.yml" ]; then
   wget http://127.0.0.1:8899/share/wukongdata/config.yml
   cp -f config.yml /root/.wukong/config.yml
 fi
-if [ ! -f "/root/.wukong/xiaotianquan.pmdl" ]; then
-  wget http://127.0.0.1:8899/share/wukongdata/xiaotianquan.pmdl
-  cp -f xiaotianquan.pmdl /root/.wukong/xiaotianquan.pmdl
+if [ ! -f "/root/.wukong/huanxing.pmdl" ]; then
+  wget http://127.0.0.1:8899/share/wukongdata/huanxing.pmdl
+  cp -f huanxing.pmdl /root/.wukong/huanxing.pmdl
 fi
 # wget http://127.0.0.1:8899/share/wukongdata/config.yml
 # wget http://127.0.0.1:8899/share/wukongdata/wukong.pmdl
