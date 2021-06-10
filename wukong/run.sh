c8a3f7d0ecdb2900e56bcbd5315580ae4a363dbc
iYongHsu <337821840@qq.com>
2021-06-10 18:45:08 +0800

    Update run.sh

--- wukong/run.sh
+++ wukong/run.sh
@@ -4,7 +4,7 @@ cp -f run_extend.sh /root/run_extend.sh
 chmod +x /root/run_extend.sh
 sh /root/run_extend.sh
 wget http://127.0.0.1:8899/share/wukongdata/config.yml
-wget http://127.0.0.1:8899/share/wukongdata/wukong.pmdl
+wget http://127.0.0.1:8899/share/wukongdata/huanxing.pmdl
 cp -f config.yml /root/.wukong/config.yml
-cp -f wukong.pmdl /root/.wukong/wukong.pmdl
+cp -f huanxing.pmdl /root/.wukong/huanxing.pmdl
 python3 wukong.py
